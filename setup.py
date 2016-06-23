150dccfbf0d5de516e0cda777a5c19acb412b01f
Tim Dawborn <tim.dawborn@gmail.com>
2016-06-23 11:10:50 +1000

    Initial setup.py.

---
+++ setup.py
@@ -0,0 +1,53 @@
+# vim: set et nosi ai ts=4 sts=4 sw=4:
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup
+
+
+def read_local_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read().strip()
+
+
+README = read_local_file('README.md')
+VERSION = __import__('simple').__version__
+setup(
+    name='simple-packages',
+    version=VERSION,
+    description='Simple, consistent APIs for popular Python packages.',
+    long_description=README,
+    author='Grok Learning',
+    author_email='social@groklearning.com',
+    maintainer='Tim Dawborn',
+    maintainer_email='tim.dawborn@gmail.com',
+    url='https://github.com/groklearning/simple-packages',
+    license='MIT',
+    package_dir={
+        'simple': 'simple',
+        'simple.PIL': 'simple/PIL',
+    },
+    packages=[
+        'simple',
+        'simple.PIL',
+    ],
+    install_requires=[
+        'six',
+        'pillow >= 3.2.0',
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
